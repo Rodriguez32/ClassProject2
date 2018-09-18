f4b598a0c941b85688e265343046fd5ae0b7d1d4
mandoh <mandomartinez2017@gmail.com>
2018-09-17 20:55:47 -0500

    rough draft of a schema.

--- MusicApp/models/schema.sql
+++ MusicApp/models/schema.sql
@@ -1,5 +1,37 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS appdb;
+CREATE DATABASE appdb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE appdb
+
+CREATE TABLE users (
+  id INT NOT NULL,
+  user_name VARCHAR(100) NULL,
+  password VARCHAR(100) NULL,
+  email VARCHAR(100) NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM users;
+
+CREATE TABLE creators (
+  contract_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  PRIMARY KEY (contract_id)
+);
+
+CREATE TABLE receipients (
+  contract_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  PRIMARY KEY (contract_id)
+);
+
+CREATE TABLE contracts (
+  id INT NOT NULL,
+  contract_id NOT NULL,
+  type VARCHAR(100) NULL,
+  date TIMESTAMP NOT NULL default current_timestamp,
+  FOREIGN KEY (contract_id) REFERENCES receipients(contract_id)
+  PRIMARY KEY (id)
+);
