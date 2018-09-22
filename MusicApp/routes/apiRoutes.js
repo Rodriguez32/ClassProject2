2e208947fcf280130a65022ae722f028c4fb04c8
Ursus <ugurungsus@gmail.com>
2018-09-22 12:51:21 -0500

    fixed linting issues in apiRoutes and contracts.js

--- MusicApp/routes/apiRoutes.js
+++ MusicApp/routes/apiRoutes.js
@@ -3,23 +3,22 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // GET route for getting all of the users
   app.get("/api/users/", function(req, res) {
-    db.user.findAll({})
-      .then(function(users) {
-        res.json(users);
-      });
+    db.user.findAll({}).then(function(users) {
+      res.json(users);
+    });
   });
 
   // Get route for returning all contracts that you are a part of
   app.get("/api/contracts/:email", function(req, res) {
-    db.contracts.findAll({
-      where: {
-        creator_id: req.params.email,
-        receiver_id: req.params.email
-      }
-    })
+    db.contracts
+      .findAll({
+        where: {
+          creator_id: req.params.email,
+          receiver_id: req.params.email
+        }
+      })
       .then(function(myContracts) {
         res.json(myContracts);
       });
@@ -28,36 +27,38 @@ module.exports = function(app) {
   // POST route for adding a new user
   app.post("/api/users", function(req, res) {
     console.log(req.body);
-    db.user.create({
-      user_name: req.body.user_name,
-      email: req.body.email
-    })
+    db.user
+      .create({
+        user_name: req.body.user_name,
+        email: req.body.email
+      })
       .then(function(newUser) {
         res.json(newUser);
       });
   });
 
-   // POST route for adding a new contract
-   app.post("/api/contracts", function(req, res) {
+  // POST route for adding a new contract
+  app.post("/api/contracts", function(req, res) {
     console.log(req.body);
-    db.contracts.create({
-      creator_id: req.body.creator_email,
-      receiver_id: req.body.receiver_email,
-      contract_text: req.body.text
-    })
+    db.contracts
+      .create({
+        creator_id: req.body.creator_email,
+        receiver_id: req.body.receiver_email,
+        contract_text: req.body.text
+      })
       .then(function(newContract) {
         res.json(newContract);
       });
   });
 
-
   // DELETE route for deleting posts
   app.delete("/api/contracts/:id", function(req, res) {
-    db.user.destroy({
-      where: {
-        contract_id: req.params.id
-      }
-    })
+    db.user
+      .destroy({
+        where: {
+          contract_id: req.params.id
+        }
+      })
       .then(function(contracts) {
         res.json(contracts);
       });
@@ -65,8 +66,8 @@ module.exports = function(app) {
 
   // PUT route for updating contracts when user accepts contract
   app.put("/api/contracts", function(req, res) {
-    db.user.update(req.body,
-      {
+    db.user
+      .update(req.body, {
         where: {
           receiver_id: req.body.email
         }
