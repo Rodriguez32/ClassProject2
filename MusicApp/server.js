48b25a4e4fd6435c8552a90a4c4550842296d0d7
Phenom Phons <alphonstate@gmail.com>
2018-09-26 13:54:46 -0500

    front-end work & passport

--- MusicApp/server.js
+++ MusicApp/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require("passport");
+var session = require("express-session");
 
 var db = require("./models");
 
@@ -12,6 +14,15 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+); //Session Secret
 
 // Handlebars
 app.engine(
@@ -25,8 +36,8 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-require("./public/js/contractpdf");
-runPdf();
+require("./routes/auth.js")(app, passport);
+require("./config/passport.js")(passport, db.user);
 
 var syncOptions = { force: false };
 
@@ -48,3 +59,11 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
+
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
